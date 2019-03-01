380227ec7c9326f25a939c439dcb715577071751
AlexFlipnote <alexander.samuelsen.manso@gmail.com>
2019-03-01 19:18:21 +0100

    domain change

--- utils/texts/responses.py
+++ utils/texts/responses.py
@@ -17,6 +17,6 @@ drunkresponse = [
     "Life is not a fairy tale. If you lose your shoe at midnight, you're drunk",
     "Me? Drunk? Pffft.. no?", "No your drunk \*hick\*", "what is life?",
     "I wanna get so drunk..", "Party is life!!1", "I drank so much vodka last night, I woke up with a russian accent",
-    "Drunk ~~is love~~\nDrunk. Just drunk.", "A man's true character comes out when he's drunk.", "https://i.alexflipnote.xyz/5dcc3a.png",
+    "Drunk ~~is love~~\nDrunk. Just drunk.", "A man's true character comes out when he's drunk.", "https://i.alexflipnote.dev/5dcc3a.png",
     "Get drunk they said.. it will be fun they said..", "🍻"
 ]
